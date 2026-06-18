934ea99f261088ffd16e6bbaf44824db8cf5d38c
DangXuan <DangXuan@quant-vm.local>
2026-06-18 09:28:40 +0000

    fix(ci): remove 3 unused type: ignore comments (mypy 3.12 compat)

--- collectors/adapters/alpaca_adapter.py
+++ collectors/adapters/alpaca_adapter.py
@@ -37,7 +37,7 @@ class AlpacaUSAdapter:
         response = self._client.get_stock_bars(request)
 
         records = []
-        for symbol, bars in response.data.items():  # type: ignore[union-attr]
+        for symbol, bars in response.data.items():
             for bar in bars:
                 records.append(
                     {
@@ -59,7 +59,7 @@ class AlpacaUSAdapter:
         from alpaca.data.requests import StockLatestBarRequest
 
         response = self._client.get_stock_latest_bar(StockLatestBarRequest(symbol_or_symbols=[]))
-        return sorted(response.data.keys())  # type: ignore[union-attr]
+        return sorted(response.data.keys())
 
     def market_hours(self, d: date) -> tuple[time, time]:
         return time(9, 30), time(16, 0)
